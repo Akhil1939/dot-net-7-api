138f0ae531a42d381dc40de868c8610a4ea2b37d
AKhil Kukadiya <akhilkukadiya464@gmail.com>
2023-07-09 10:39:36 +0530

    jwt token implemented

--- api.services/Services/AccountService.cs
+++ api.services/Services/AccountService.cs
@@ -2,21 +2,24 @@
 using api.models.DTOs;
 using api.repositories.IRepositories;
 using api.services.IServices;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AutoMapper;
+using Common;
+using Common.Utils.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace api.services.Services
 {
     public class AccountService :GenericServices<User> ,IAccountService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
 
-        public AccountService(IUnitOfWork unitOfWork) : base(unitOfWork.UserRepository)
+        public AccountService(IUnitOfWork unitOfWork, IConfiguration configuration, IMapper mapper) : base(unitOfWork.UserRepository)
         {
             _unitOfWork = unitOfWork;
+            _configuration = configuration;
+            _mapper = mapper;
         }
         public TokenDTO Login(LoginDTO dto)
         {
@@ -26,10 +29,12 @@ namespace api.services.Services
             {
                 throw new Exception("Invalid credentials");
             }
+            JwtSetting jwtSetting = GetJwtSetting();
+            SessionUserModel currentUser = _mapper.Map<SessionUserModel>(user);
 
             return new TokenDTO()
             {
-                Token = "token",
+                Token = JwtHelper.GenerateToken(jwtSetting, currentUser),
                 UserRole = user.Role
             };
 
@@ -41,6 +46,13 @@ namespace api.services.Services
             return dto.Password == user.Password;
 
            
+        }
+
+        private JwtSetting GetJwtSetting()
+        {
+            JwtSetting jwtSetting = new();
+            _configuration.GetSection("JwtSetting").Bind(jwtSetting);
+            return jwtSetting;
         }
     }
 }
